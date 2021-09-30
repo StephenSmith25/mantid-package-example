e107526ef965490cb98f8b82b322709705948e18
Smith <stephen.smith@stfc.ac.uk>
2021-09-30 15:31:14 +0100

    add gui entry point

--- src/setup.py
+++ src/setup.py
@@ -3,5 +3,10 @@ from setuptools import find_packages, setup
 setup(
     name='m_window', 
     version="0.1.0",
-    packages=find_packages(exclude=['*.test'])
+    packages=find_packages(exclude=['*.test']),
+    entry_points={
+        "gui_scripts": [
+            "m_window = m_window.main:main"
+        ]
+    },
     )
