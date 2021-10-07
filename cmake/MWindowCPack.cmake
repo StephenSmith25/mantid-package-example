d107fd5b417cc234f9a3b9f15d409998f4a0868b
Stephen Smith <stephen.smith@stfc.ac.uk>
2021-10-07 11:21:11 +0100

    Experimental osx packaging finished..

--- cmake/MWindowCPack.cmake
+++ cmake/MWindowCPack.cmake
@@ -1,4 +1,3 @@
-
 set(CPACK_PACKAGE_NAME "MWindow")
 set(CPACK_PACKAGE_FILE_NAME "MWindow")
 set(CPACK_PACKAGE_VENDOR "Stephen Smith")
@@ -10,8 +9,16 @@ set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "Test project for Mantid")
 set(CPACK_PACKAGE_VERSION_MAJOR 0)
 set(CPACK_PACKAGE_VERSION_MINOR 1)
 set(CPACK_PACKAGE_VERSION_PATCH 0)
-set(CPACK_POSTFLIGHT_RUNTIME_SCRIPT ${PROJECT_SOURCE_DIR}/bundle-scripts/postinstall)
+set(CPACK_POSTFLIGHT_RUNTIME_SCRIPT
+    ${PROJECT_SOURCE_DIR}/bundle-scripts/postinstall)
 set(CPACK_GENERATOR "productbuild")
 set(CPACK_DEBIAN_PACKAGE_MAINTAINER "Stephen Smith") # required
 
+
 include(CPack)
+
+cpack_add_component(
+  Runtime
+  DISPLAY_NAME Runtime
+  DESCRIPTION "Runtime of window application" PLIST
+                        ${PROJECT_SOURCE_DIR}/packaging/mwindow.plist)
