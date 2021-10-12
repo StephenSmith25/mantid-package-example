f63f1a752dc0e53128846887afbc474a83c4b280
Stephen <stephen.smith@stfc.ac.uk>
2021-10-12 13:20:01 +0100

    Nsis installer works, but no custom postinstall yet

--- cmake/MantidPrototypeCPack.cmake
+++ cmake/MantidPrototypeCPack.cmake
@@ -25,17 +25,20 @@ cpack_add_component(
                         ${PROJECT_SOURCE_DIR}/packaging/MantidPrototype.plist)
 
 elseif(WIN32)
-set(CPACK_GENERATOR "NSIS")
 
+set(CPACK_GENERATOR "NSIS")
+set(CPACK_PACKAGE_INSTALL_REGISTRY_KEY "MantidPrototype")
+
+set(CPACK_NSIS_INSTALLED_ICON_NAME "${APP_LOW_NAME}.ico")
+set(CPACK_NSIS_HELP_LINK "www.google.co.uk")
+set(CPACK_NSIS_URL_INFO_ABOUT "www.google.co.uk")
+set(CPACK_NSIS_CONTACT "1@2")
 include(CPack)
 
-cpack_add_component(
-  Runtime
-  DISPLAY_NAME Runtime
-  DESCRIPTION "Runtime of window application" PLIST
-                        ${PROJECT_SOURCE_DIR}/packaging/MantidPrototype.plist)
-
-elseif(WIN32)
-
+# Configure file with custom definitions for NSIS.
+configure_file(
+  ${PROJECT_SOURCE_DIR}/bundle-scripts/NSIS.definitions.nsh.in
+  ${CMAKE_CURRENT_BINARY_DIR}/resources/nsis/NSIS.definitions.nsh
+)
 
 endif()
